2509644decd598b85225d76c57c90c283a5b93b8
leooliveira <leooliveira@equinix.com>
2022-07-20 10:17:46 -0300

    fix background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        blur: 'url(/src/assets/blur-background.png)'
-      },
       fontFamily: {
         sans: 'Roboto, sans-serif'
       },
